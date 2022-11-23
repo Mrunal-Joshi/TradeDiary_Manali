e9ea1b2b362ff0cf2028e0fd7cb73b885ea7dde1
Mrunal Narendra Joshi <mrunal.joshi@Mrunals-MacBook-Air.local>
2022-11-23 06:22:59 +0530

    Started working on Tradesheet

--- TradeSheet/views.py
+++ TradeSheet/views.py
@@ -4,5 +4,10 @@ from django.http import HttpResponse
 from .models import TradeSheet
 # Create your views here.
 def displaysheet(request):
-    sheet = 
-    return render(request,'tradesheet.html')
+    # <QuerySet [{'id': 1, 'date': datetime.date(2022, 11, 16), 
+    # 'symbol': 'FISV', 'no_of_shares': 12, 'buy_price': 110.0, 'sell_price': 11.0, 'profit_loss': 0.0}
+    sheet = TradeSheet.objects.all().values()
+    # for i in sheet:
+    #     print(i['profit_loss']=i['sell_price']-i['buy_price'])
+
+    return render(request,'tradesheet.html',{'sheet':sheet})
